c4f3e97b74b94e8165005c8c820d094eca2d8ac3
zakitaku <b1016042@fun.ac.jp>
2018-10-03 11:15:55 +0900

    お金の概念を追加

--- Player.py
+++ Player.py
@@ -11,12 +11,15 @@ from BlackJack.GamePlayer import *
 
 class Player(GamePlayer):
     # プレイヤーの初期化
-    def __init__(self, name, tag="player"):
+    def __init__(self, name, money=1000000, betMoney=0, tag="player"):
         self.name = name
+        self.money = money
+        self.betMoney = betMoney
         self.totalwin = 0
         self.totallose = 0
         self.totaldraw = 0
         self.tag = tag
+        self.debagtxt = ""
         super().__init__()
 
     # プレイヤーにカードを配るときに使用する関数
@@ -27,34 +30,39 @@ class Player(GamePlayer):
     # プレイヤー側のヒットの処理
     def hit(self, dealer):
         self.dealedcard(dealer.dealcard())
-        self.totalvalue()
-        self.totalvalue()
+        self.debagtxt += "H"
 
     # プレイヤー側のスタンドの処理
     def stand(self):
+        self.debagtxt += "S"
         pass
 
     # プレイヤ－側のダブルダウンの処理
     def doubledown(self, dealer):
+        self.debagtxt += "D("
+        self.betMoney *= 2
         self.hit(dealer)
+        self.stand()
+        self.debagtxt += ")"
 
-    # プレイヤー側のスプリットの処理
-    def split(self):
-        pass
+    # プレイヤー側のベットの処理
+    def bet(self, betMoney):
+        self.betMoney = betMoney
 
     # 自身の手札を表示するUI
     def showhands(self):
-        # print("---hands---")
         for x in self.cards:
              print('/', x.suit, x.rank)
         print("---total---: ", self.total, "\n")
 
     # プレイヤーの勝利回数を増やす
-    def addtotalwin(self):
+    def addtotalwin(self, money):
+        self.money += money
         self.totalwin += 1
 
     # プレイヤーの敗北回数を増やす
-    def addtotallose(self):
+    def addtotallose(self, money):
+        self.money -= money
         self.totallose += 1
 
     # プレイヤーの引き分け回数を増やす
